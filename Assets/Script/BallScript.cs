e0486acb9e366ae89431852407c58ecbf071faab
[developer119] <[developer119korea@gmail.com]>
2016-07-25 17:59:05 +0900

    코루틴을 활용하여 일정시간마다 볼을 생성하도록 함
    1초~3초사이로 볼을 생성하고 화면에서 떨어지면 삭제하도록 하였다

---
+++ Assets/Script/BallScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour
+{
+    public static List<BallScript> allBalls = new List<BallScript>();
+	
+    private void Awake()
+    {
+        allBalls.Add(this);
+    }
+    
+    private void OnDestroy()
+    {
+        allBalls.Remove(this);
+    }
+
+	// Update is called once per frame
+	private void Update ()
+    {
+        if (transform.position.y < -5)
+        Destroy(gameObject);
+	}
+}
---
+++ Assets/Script/CubeScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour
+{
+    public Transform ball_;
+
+    // Use this for initialization
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            Transform newBall = Instantiate(ball_, transform.position, Quaternion.identity) as Transform;
+            newBall.rigidbody.AddForce(new Vector3(Random.value * 2000 - 1000, 2000, Random.value * 2000 - 1000));
+            yield return new WaitForSeconds(1 + Random.value * 2);
+        }
+    }
+}
+
