3cd404bb9b871e5ab6a65ddaeec00b9952460683
Youngwan Cho <Youngwan Cho>
2016-07-25 23:29:20 +0900

    Time.timeScale을 이용하여 카메라포커스가 이동하는 볼을 정지시키고 카메라포커스 코루틴이 끝나면 다시 재개 하는 코드 추가

--- Assets/Script/CubeScript.cs
+++ Assets/Script/CubeScript.cs
@@ -31,11 +31,12 @@ public class CubeScript : MonoBehaviour
     {
         float t = 0f;
         Vector3 originalPosition = objectToMove.position;
+        float lastTime = Time.realtimeSinceStartup;
 
         while (t < 1)
         {
             objectToMove.position = Vector3.Lerp(originalPosition, position, t);
-            t += Time.deltaTime / time;
+            t += (Time.realtimeSinceStartup - lastTime) / time;
             yield return null;
         }
     }
@@ -43,18 +44,36 @@ public class CubeScript : MonoBehaviour
     private IEnumerator CutSequence()
     {
         _runningSequence = true;
+        Time.timeScale = 0;
 
         Vector3 originalPoisiton = Camera.main.transform.position;
 
         foreach (BallScript ball in BallScript.allBalls.ToArray())
         {
-            Vector3 targetPosition = ball.transform.position - Vector3.forward;
+            Vector3 targetPosition = ball.transform.position - (Vector3.forward * 3);
             yield return StartCoroutine(MoveObject(Camera.main.transform, targetPosition, 2));
-            yield return new WaitForSeconds(0.5f);
+            yield return WaitForRealSeconds(0.5f);
         }
 
         yield return StartCoroutine(MoveObject(Camera.main.transform, originalPoisiton, 2));
+        yield return WaitForRealSeconds(2.0f);
+
+        Time.timeScale = 1;
         _runningSequence = false;
+    }
+
+    private Coroutine WaitForRealSeconds(float time)
+    {
+        return StartCoroutine(Wait(time));
+    }
+
+    private IEnumerator Wait(float time)
+    {
+        float current = Time.realtimeSinceStartup;
+        while(Time.realtimeSinceStartup - current < time)
+        {
+            yield return null;
+        }
     }
 }
 
