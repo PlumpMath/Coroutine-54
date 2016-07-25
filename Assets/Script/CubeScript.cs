1c1b31febe191a5a79e52df9500bbc6a651149c5
[developer119] <[developer119korea@gmail.com]>
2016-07-25 18:03:31 +0900

    카메라 무빙을 이용하여 생성된 볼을 클로즈업 하는 기능 추가
    Retrieve the balls 버튼을 클릭하면 살아있느 모든 볼을 리스트에 담아서 순차적으로 카메라 무빙을 시도하는데
    무빙중에 제거된 볼을 참조하면 에러가난다.

--- Assets/Script/CubeScript.cs
+++ Assets/Script/CubeScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CubeScript : MonoBehaviour
 {
     public Transform ball_;
-
+    private bool _runningSequence;
     // Use this for initialization
     private IEnumerator Start()
     {
@@ -15,5 +15,47 @@ public class CubeScript : MonoBehaviour
             yield return new WaitForSeconds(1 + Random.value * 2);
         }
     }
+
+    private void OnGUI()
+    {
+        if (_runningSequence)
+            return;
+
+        if (GUILayout.Button("Retrieve the balls"))
+        {
+            StartCoroutine(CutSequence());
+        }
+    }
+
+    private IEnumerator MoveObject(Transform objectToMove, Vector3 position, float time)
+    {
+        float t = 0f;
+        Vector3 originalPosition = objectToMove.position;
+
+        while (t < 1)
+        {
+            objectToMove.position = Vector3.Lerp(originalPosition, position, t);
+            t += Time.deltaTime / time;
+            yield return null;
+        }
+    }
+
+    private IEnumerator CutSequence()
+    {
+        _runningSequence = true;
+
+        Vector3 originalPoisiton = Camera.main.transform.position;
+
+        foreach (BallScript ball in BallScript.allBalls.ToArray())
+        {
+            Vector3 targetPosition = ball.transform.position - Vector3.forward;
+            yield return StartCoroutine(MoveObject(Camera.main.transform, targetPosition, 2));
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        yield return StartCoroutine(MoveObject(Camera.main.transform, originalPoisiton, 2));
+        _runningSequence = false;
+    }
 }
+
 
